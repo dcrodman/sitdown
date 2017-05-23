e16fedc42e7fa54c297dfa44b921c75d5c9634ff
Drew Rodman <arodman@pandora.com>
2017-05-23 11:44:22 -0700

    this might not work

--- controller.go
+++ controller.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	//"github.com/stianeikeland/go-rpio"
+	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
+	if err := rpio.Open(); err != nil {
+		panic(err)
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(21)
+	for i := 0; i < 5; i++ {
+		pin.PullDown()
+	}
+
 	fmt.Println("It works")
 }
