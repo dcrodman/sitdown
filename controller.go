d5f32761faad13efa89df5d92e68abbfe4cf576f
Scott Kirkpatrick <scott.kirkpatrick4@gmail.com>
2017-05-23 22:45:37 +0000

    Add more server capabilities, option to disable server

--- controller.go
+++ controller.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"strconv"
 	"time"
+	"sync"
+	"os"
 )
 
 const (
@@ -14,11 +16,23 @@ const (
 	pinButtonDown rpio.Pin = rpio.Pin(12)
 )
 
+var (
+	moveMux = &sync.Mutex{}
+	server = false
+)
+
 func main() {
 	if err := rpio.Open(); err != nil {
 		panic(err)
 	}
 	defer rpio.Close()
+
+	for _, arg := range os.Args {
+		switch (arg) {
+		case "-s", "--server":
+			server = true
+		}
+	}
 
 	raise()
 	sleep(2000)
@@ -28,14 +42,37 @@ func main() {
 	sleep(2000)
 	stopLowering()
 
+	if server {
+		http.HandleFunc("/move", HandleMove)
+		http.HandleFunc("/set", HandleSet)
+		http.ListenAndServe(":8080", nil)
+	}
+
 	pinButtonUp.PullOff()
 	pinButtonDown.PullOff()
-
-	http.HandleFunc("/move", HandleMove)
-	http.ListenAndServe(":8080", nil)
 }
 
 func HandleMove(responseWriter http.ResponseWriter, request *http.Request) {
+	vals, err := url.ParseQuery(request.URL.RawQuery)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	direction := vals["direction"][0]
+	if direction != "down" && direction != "up" {
+		fmt.Printf("Invalid direction: %s\n", direction)
+		return
+	}
+	time, err := strconv.Atoi(vals["time"][0])
+	if err != nil || time < 0 || time > 10000 {
+		fmt.Printf("Invalid time: %d\n", time)
+		return
+	}
+
+	fmt.Printf("Received move command: %s %d\n", direction, time)
+}
+
+func HandleSet(responseWriter http.ResponseWriter, request *http.Request) {
 	vals, err := url.ParseQuery(request.URL.RawQuery)
 	if err != nil {
 		fmt.Println(err)
@@ -47,7 +84,22 @@ func HandleMove(responseWriter http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	fmt.Printf("Received move command: %d\n", height)
+	fmt.Printf("Received set command: %d\n", height)
+}
+
+func move(direction string, time int) {
+	moveMux.Lock()
+	defer moveMux.Unlock()
+	switch(direction) {
+	case "up":
+		raise()
+		sleep(time)
+		stopRaising()
+	case "down":
+		lower()
+		sleep(time)
+		stopLowering()
+	}
 }
 
 func sleep(ms int) {
