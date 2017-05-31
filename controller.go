0baf6029f7fbf0471dc6186a56dc977e3261604a
Drew Rodman <arodman@pandora.com>
2017-05-31 16:26:58 -0700

    Move HTTP handler into its own file

--- controller.go
+++ controller.go
@@ -8,8 +8,6 @@ import (
 	"github.com/dcrodman/sitdown/desk"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 	"strconv"
@@ -56,13 +54,7 @@ func main() {
 	StartSubscriber(DeskCommandHandler)
 	StartAnnouncing()
 
-	http.HandleFunc("/move", HandleMove)
-	http.HandleFunc("/set", HandleSet)
-	http.HandleFunc("/height", HandleHeight)
-	logger.Println("Starting HTTP server")
-	if err := http.ListenAndServe(":"+*port, nil); err != nil {
-		panic(err)
-	}
+	StartHTTPEndpoint(*port)
 }
 
 func readConfig() {
@@ -143,45 +135,6 @@ func CommandModeSubscribeHandler(message Message) {
 		logger.Printf("Discovered controller %s (id: %s)\n", message.IPAddr, message.Id)
 		addKnownController(message.Id, message.IPAddr)
 	}
-}
-
-// Handler method for HTTP requests sent to /move.
-func HandleMove(responseWriter http.ResponseWriter, request *http.Request) {
-	vals, err := url.ParseQuery(request.URL.RawQuery)
-	if err != nil {
-		logger.Println(err)
-		return
-	}
-	direction := vals["direction"][0]
-	if direction != "down" && direction != "up" {
-		logger.Printf("Invalid direction: %s\n", direction)
-		return
-	}
-	duration, err := strconv.Atoi(vals["time"][0])
-	if err != nil || duration < 0 || duration > 10000 {
-		logger.Printf("Invalid time: %d\n", duration)
-		return
-	}
-
-	logger.Printf("Received move command: %s %d\n", direction, duration)
-	move(direction, duration)
-	fmt.Fprintf(responseWriter, "Moved to %.1f", desk.Height())
-}
-
-// Handler method for HTTP requests sent to /set.
-func HandleSet(responseWriter http.ResponseWriter, request *http.Request) {
-	vals, err := url.ParseQuery(request.URL.RawQuery)
-	if err != nil {
-		logger.Println(err)
-		return
-	}
-	setHeight(vals["height"][0])
-	fmt.Fprintf(responseWriter, "Changed to %.1f", desk.Height())
-}
-
-// Handler method for HTTP requests sent to /height.
-func HandleHeight(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(responseWriter, "%.1f", desk.Height())
 }
 
 // Command handler that should be running on the actual desk controllers.
